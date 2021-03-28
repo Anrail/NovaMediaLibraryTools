a472c7b7586b64ce2a5c2b62561b736be06cb8bd
Александр Маловичко <medius.prod@gmail.com>
2021-03-29 00:15:14 +0300

    Testing

--- src/MediaCast.php
+++ src/MediaCast.php
@@ -5,7 +5,7 @@
  * Licensed under MIT (https://github.com/Anrail/NovaMediaLibraryTools/blob/main/LICENSE)
  */
 
-namespace Anrail\NovaMediaLibraryTools;
+namespace Anrail\Tool;
 
 
 use Illuminate\Support\Facades\App;
